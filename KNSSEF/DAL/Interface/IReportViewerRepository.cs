49adba145fc0f50703e4288983108e6030fcd9e8
N Hadi <nurul.hadi@sakura-system.co.id>
2016-11-29 09:14:43 +0700

    Report,

---
+++ KNSSEF/DAL/Interface/IReportViewerRepository.cs
@@ -0,0 +1,19 @@
+﻿using KNSSEF.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KNSSEF.DAL.Interface
+{
+    public interface IReportViewerRepository : IDALRepository<Group>
+    {
+        /// <summary>
+        /// Get Data By Id Group
+        /// </summary>
+        /// <param name="groupId"></param>
+        /// <returns></returns>
+        Group GetDataById(string groupId);
+    }
+}
---
+++ KNSSService/Interface/IReportViewerService.cs
@@ -0,0 +1,60 @@
+﻿using KNSSEF.Model;
+using KNSSService.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KNSSService.Interface
+{
+    #region Service Definition
+
+    public interface IReportViewerService
+    {
+        /// <summary>
+        /// Get All Data ReportViewer
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        GetAllDataReportViewerResponse GetAllDataReportViewer();
+
+        /// <summary>
+        /// Get Data ReportViewer By Id
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        GetDataReportViewerByIdResponse GetDataReportViewerById(GetDataReportViewerByIdRequest request);
+
+    }
+
+    #endregion
+
+    #region Message
+
+    public class GetAllDataReportViewerRequest
+    {
+
+    }
+
+    public class GetAllDataReportViewerResponse : ResponseBase
+    {
+        private List<Group> _ReportViewerList;
+
+        public List<Group> ReportViewerList
+        {
+            get { return _ReportViewerList ?? (_ReportViewerList = new List<Group>()); }
+        }
+    }
+
+    public class GetDataReportViewerByIdRequest
+    {
+        public string ReportViewerId { get; set; }
+    }
+
+    public class GetDataReportViewerByIdResponse : ResponseBase
+    {
+        public Group ReportViewer { get; set; }
+    }
+    #endregion
+}
