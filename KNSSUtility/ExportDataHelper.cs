b0e0b8ca280db369caeccc92d2d3b6c6bf5f244a
N Hadi <nurul.hadi@sakura-system.co.id>
2016-11-30 16:20:31 +0700

    sadasd

--- KNSSUtility/ExportDataHelper.cs
+++ KNSSUtility/ExportDataHelper.cs
@@ -16,129 +16,131 @@ namespace KNSSUtility
             //
         }
 
-        //public static Dictionary<string, string> ExportToExcel(string fileName, string worksheetName, string header, string sql, List<string> isCenter = null)
-        //{
-        //    Dictionary<string, string> dt = new Dictionary<string, string>();
-        //    string sourcePath = Conf.DocumentFolder + fileName;
-        //    string columnName = "", fldName = "", fileExcel = "", startSheet = "", endSheet = "";
-        //    int iSheetValue = 0;
-        //    #region temp variable
-        //    DateTime tmpDate; int tmpInt; decimal tmpDecimal;
-        //    #endregion
+        public static Dictionary<string, string> ExportToExcel<T>(string fileName, string worksheetName, string header, List<T> dataFromService, List<string> isCenter = null) where T : class
+        {
+            Dictionary<string, string> dt = new Dictionary<string, string>();
+            string sourcePath = Conf.DocumentFolder + fileName;
+            string columnName = "", fldName = "", fileExcel = "", startSheet = "", endSheet = "";
+            int iSheetValue = 0;
+            #region temp variable
+            DateTime tmpDate; int tmpInt; decimal tmpDecimal;
+            #endregion
 
-        //    if (worksheetName == String.Empty)
-        //    {
-        //        worksheetName = "Sheet1";
-        //    }
+            if (worksheetName == String.Empty)
+            {
+                worksheetName = "Sheet1";
+            }
 
-        //    var allData = Db.GetMultiDataColName(Db.Query(sql));
+            var allData = dataFromService;
 
-        //    Dictionary<string, object> data2 = new Dictionary<string, object>();
+            Dictionary<string, object> data2 = new Dictionary<string, object>();
 
-        //    //new workbook
-        //    XLWorkbook workBook = new XLWorkbook();
-        //    //worksheet
-        //    var sheet = workBook.Worksheets.Add(worksheetName);
+            //new workbook
+            XLWorkbook workBook = new XLWorkbook();
+            //worksheet
+            var sheet = workBook.Worksheets.Add(worksheetName);
 
-        //    var data = SqlForAdminHelper.GetFieldAndType(sql);
-        //    List<Dictionary<string, string>> col = new List<Dictionary<string, string>>();
-        //    foreach (var items in data)
-        //    {
-        //        Dictionary<string, string> tmpData = new Dictionary<string, string>();
-        //        tmpData.Add("fldName", items["fldName"]);
-        //        tmpData.Add("fldType", items["fldType"]);
-        //        col.Add(tmpData);
-        //    }
+            var data = Library.GetFieldAndType(typeof(T));
+            List<Dictionary<string, string>> col = new List<Dictionary<string, string>>();
+            foreach (var items in data)
+            {
+                Dictionary<string, string> tmpData = new Dictionary<string, string>();
+                tmpData.Add("fldName", items["fldName"]);
+                tmpData.Add("fldType", items["fldType"]);
+                col.Add(tmpData);
+            }
 
-        //    sheet.Cell("A1").Value = header;
-        //    sheet.Cell("A1").Style.Font.Bold = true;
-        //    sheet.Cell("A1").Style.Font.FontSize = 16;
+            sheet.Cell("A1").Value = header;
+            sheet.Cell("A1").Style.Font.Bold = true;
+            sheet.Cell("A1").Style.Font.FontSize = 16;
 
-        //    int i = 1;
-        //    int counter = 0;
-        //    foreach (Dictionary<string, string> x in col)
-        //    {
-        //        columnName = Library.GetColumnExcel(i) + "3";
-        //        fldName = x["fldName"];
-        //        sheet.Cell(columnName).Value = fldName;
-        //        iSheetValue = 4;
+            int i = 1;
+            int counter = 0;
+            foreach (Dictionary<string, string> x in col)
+            {
+                columnName = Library.GetColumnExcel(i) + "3";
+                fldName = x["fldName"];
+                sheet.Cell(columnName).Value = fldName;
+                iSheetValue = 4;
 
-        //        if (counter == 0)
-        //            startSheet = columnName;
-        //        else if (counter == col.Count - 1)
-        //            endSheet = columnName;
+                if (counter == 0)
+                    startSheet = columnName;
+                else if (counter == col.Count - 1)
+                    endSheet = columnName;
 
-        //        foreach (var row in allData)
-        //        {
-        //            columnName = Library.GetColumnExcel(i) + iSheetValue;
-        //            sheet.Cell(columnName).Value = row[fldName];
-        //            iSheetValue++;
+                int countIndex = 0;
+                foreach (var row in allData)
+                {
+                    columnName = Library.GetColumnExcel(i) + iSheetValue;
+                    var rowValue = row.GetType().GetProperty(fldName).GetValue(allData[countIndex]).ToString();
+                    sheet.Cell(columnName).Value = rowValue;
+                    iSheetValue++;
 
-        //            if (DateTime.TryParse(row[fldName], out tmpDate))
-        //            {
-        //                sheet.Cell(columnName).Style.DateFormat.Format = "dd-MMM-yyyy";
-        //                sheet.Cell(columnName).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
-        //            }
-        //            else if (decimal.TryParse(row[fldName], out tmpDecimal))
-        //            {
-        //                sheet.Cell(columnName).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Right;
-        //            }
-        //            else if (int.TryParse(row[fldName], out tmpInt))
-        //            {
-        //                sheet.Cell(columnName).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Right;
-        //            }
+                    if (DateTime.TryParse(rowValue, out tmpDate))
+                    {
+                        sheet.Cell(columnName).Style.DateFormat.Format = "dd-MMM-yyyy";
+                        sheet.Cell(columnName).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+                    }
+                    else if (decimal.TryParse(rowValue, out tmpDecimal))
+                    {
+                        sheet.Cell(columnName).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Right;
+                    }
+                    else if (int.TryParse(rowValue, out tmpInt))
+                    {
+                        sheet.Cell(columnName).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Right;
+                    }
 
-        //            if (isCenter != null)
-        //            {
-        //                foreach (var item in isCenter)
-        //                {
-        //                    if (item == fldName)
-        //                    {
-        //                        if (DateTime.TryParse(row[fldName], out tmpDate))
-        //                        {
-        //                            sheet.Cell(columnName).Style.DateFormat.Format = "dd-MMM-yyyy";
-        //                            sheet.Cell(columnName).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
-        //                        }
-        //                        else
-        //                        {
-        //                            sheet.Cell(columnName).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
-        //                        }
-        //                    }
-        //                }
-        //            }
+                    if (isCenter != null)
+                    {
+                        foreach (var item in isCenter)
+                        {
+                            if (item == fldName)
+                            {
+                                if (DateTime.TryParse(rowValue, out tmpDate))
+                                {
+                                    sheet.Cell(columnName).Style.DateFormat.Format = "dd-MMM-yyyy";
+                                    sheet.Cell(columnName).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+                                }
+                                else
+                                {
+                                    sheet.Cell(columnName).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+                                }
+                            }
+                        }
+                    }
 
-        //        }
+                }
 
-        //        counter++;
-        //        i++;
-        //    }
+                counter++;
+                i++;
+            }
 
 
-        //    var rangeHeader = sheet.Range(startSheet + ":" + endSheet);
-        //    rangeHeader.Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
-        //    rangeHeader.Style.Font.Bold = true;
+            var rangeHeader = sheet.Range(startSheet + ":" + endSheet);
+            rangeHeader.Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+            rangeHeader.Style.Font.Bold = true;
 
-        //    string start = startSheet.Remove(startSheet.Length - 1);
-        //    string end = endSheet.Remove(endSheet.Length - 1);
-        //    sheet.Range(start + "1" + ":" + end + "1").Row(1).Merge();
+            string start = startSheet.Remove(startSheet.Length - 1);
+            string end = endSheet.Remove(endSheet.Length - 1);
+            sheet.Range(start + "1" + ":" + end + "1").Row(1).Merge();
 
-        //    endSheet = endSheet.Remove(endSheet.Length - 1);
-        //    var rangeBorder = sheet.Range(startSheet + ":" + endSheet + (allData.Count + 3));
-        //    rangeBorder.Style.Border.OutsideBorder = XLBorderStyleValues.Thin;
-        //    rangeBorder.Style.Border.OutsideBorder = XLBorderStyleValues.Thin;
-        //    rangeBorder.Style.Border.InsideBorder = XLBorderStyleValues.Thin;
+            endSheet = endSheet.Remove(endSheet.Length - 1);
+            var rangeBorder = sheet.Range(startSheet + ":" + endSheet + (allData.Count + 3));
+            rangeBorder.Style.Border.OutsideBorder = XLBorderStyleValues.Thin;
+            rangeBorder.Style.Border.OutsideBorder = XLBorderStyleValues.Thin;
+            rangeBorder.Style.Border.InsideBorder = XLBorderStyleValues.Thin;
 
-        //    sheet.Rows().AdjustToContents();
-        //    sheet.Columns().AdjustToContents();
+            sheet.Rows().AdjustToContents();
+            sheet.Columns().AdjustToContents();
 
-        //    fileExcel = fileName;
-        //    workBook.SaveAs(sourcePath);
+            fileExcel = fileName;
+            workBook.SaveAs(sourcePath);
 
-        //    dt.Add("fileExcel", fileExcel);
+            dt.Add("fileExcel", fileExcel);
 
-        //    return dt;
+            return dt;
 
-        //}
+        }
         public static Dictionary<string, string> ExportToExcel<T>(string fileName, string worksheetName, List<T> dataFromService, List<string> isCenter = null) where T : class
         {
             Dictionary<string, string> dt = new Dictionary<string, string>();
@@ -187,22 +189,24 @@ namespace KNSSUtility
                 else if (counter == col.Count - 1)
                     endSheet = columnName;
 
+                int countIndex = 0;
                 foreach (var row in allData)
-                {
+                {                    
                     columnName = Library.GetColumnExcel(i) + iSheetValue;
-                    sheet.Cell(columnName).Value = row;
+                    var rowValue = row.GetType().GetProperty(fldName).GetValue(allData[countIndex]).ToString();
+                    sheet.Cell(columnName).Value = rowValue;
                     iSheetValue++;
 
-                    if (DateTime.TryParse(row.ToString(), out tmpDate))
+                    if (DateTime.TryParse(rowValue, out tmpDate))
                     {
                         sheet.Cell(columnName).Style.DateFormat.Format = "dd-MMM-yyyy";
                         sheet.Cell(columnName).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
                     }
-                    else if (decimal.TryParse(row.ToString(), out tmpDecimal))
+                    else if (decimal.TryParse(rowValue, out tmpDecimal))
                     {
                         sheet.Cell(columnName).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Right;
                     }
-                    else if (int.TryParse(row.ToString(), out tmpInt))
+                    else if (int.TryParse(rowValue, out tmpInt))
                     {
                         sheet.Cell(columnName).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Right;
                     }
@@ -213,7 +217,7 @@ namespace KNSSUtility
                         {
                             if (item == fldName)
                             {
-                                if (DateTime.TryParse(row.ToString(), out tmpDate))
+                                if (DateTime.TryParse(rowValue, out tmpDate))
                                 {
                                     sheet.Cell(columnName).Style.DateFormat.Format = "dd-MMM-yyyy";
                                     sheet.Cell(columnName).Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
@@ -225,6 +229,7 @@ namespace KNSSUtility
                             }
                         }
                     }
+                    countIndex++;
                 }
 
                 counter++;
@@ -246,7 +251,7 @@ namespace KNSSUtility
             sheet.Columns().AdjustToContents();
 
             fileExcel = fileName;
-            //workBook.SaveAs(@"C:\Users\Sakura-Hadi\Desktop\tmp20160803\test.xlsx");
+            workBook.SaveAs(@"C:\Users\Sakura-Hadi\Desktop\tmp20160803\test.xlsx");
 
             dt.Add("fileExcel", fileExcel);
 
